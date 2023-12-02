b9ec97f347525f1223de0d7fde6c8478f4ffb1a4
Velizar Hristov <velizar.hs@gmail.com>
2023-12-02 13:36:36 +0200

    Add Day 2

---
+++ D2.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode2023
+{
+    internal class D2
+    {
+        public static void Run()
+        {
+            var LIMITS = new Dictionary<string, int>()
+            {
+                { "red", 12 },
+                { "green", 13 },
+                { "blue", 14 }
+            };
+
+            int sum = File.ReadLines("inputs/2").Select(line =>
+            {
+                var remaining = line.SkipWhile(c => c != ':').Skip(2);
+                while (remaining.Count() > 0)
+                {
+                    string countStr = string.Concat(remaining.TakeWhile(char.IsDigit));
+                    int count = int.Parse(countStr);
+                    string color = string.Concat(remaining.Skip(countStr.Length + 1).TakeWhile(char.IsLetter));
+                    if (LIMITS[color] < count)
+                        return 0;
+                    remaining = remaining.SkipWhile(c => c != ',' && c != ';').Skip(2);
+                }
+
+                return int.Parse(string.Concat(line.Skip(5).TakeWhile(char.IsDigit)));
+            }).Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ D2_2.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode2023
+{
+    internal class D2_2
+    {
+        public static void Run()
+        {
+            int sum = File.ReadLines("inputs/2").Select(line =>
+            {
+                var maxes = new Dictionary<string, int>()
+                {
+                    { "red", 0 },
+                    { "green", 0 },
+                    { "blue", 0 }
+                };
+                var remaining = line.SkipWhile(c => c != ':').Skip(2);
+                while (remaining.Count() > 0)
+                {
+                    string countStr = string.Concat(remaining.TakeWhile(char.IsDigit));
+                    int count = int.Parse(countStr);
+                    string color = string.Concat(remaining.Skip(countStr.Length + 1).TakeWhile(char.IsLetter));
+                    maxes[color] = Math.Max(maxes[color], count);
+                    remaining = remaining.SkipWhile(c => c != ',' && c != ';').Skip(2);
+                }
+
+                return maxes.Values.Aggregate((prod, next) => prod * next);
+            }).Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
