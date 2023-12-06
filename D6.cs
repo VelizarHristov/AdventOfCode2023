1d12611a4c4dcf8f46a45e360c6cba38f78d1504
Velizar Hristov <velizar.hs@gmail.com>
2023-12-06 07:29:57 +0200

    Add Day 6

---
+++ D6.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023
+{
+    internal class D6
+    {
+        public static void Run()
+        {
+            string[] input = File.ReadAllLines("inputs/6");
+            var times = Regex.Matches(input[0], "\\d+").Select(x => int.Parse(x.Value)).ToArray();
+            var distances = Regex.Matches(input[1], "\\d+").Select(x => int.Parse(x.Value)).ToArray();
+
+            int product = Enumerable.Range(0, times.Length).Select(idx =>
+            {
+                return Enumerable.Range(1, times[idx]).Count(heldTime =>
+                {
+                    return heldTime * (times[idx] - heldTime) > distances[idx];
+                });
+            }).Aggregate((prod, next) => prod * next);
+            Console.WriteLine(product);
+        }
+    }
+}
---
+++ D6_2.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode2023
+{
+    internal class D6_2
+    {
+        public static void Run()
+        {
+            string[] input = File.ReadAllLines("inputs/6");
+            long time = int.Parse(string.Concat(input[0].Where(char.IsDigit)));
+            long distance = long.Parse(string.Concat(input[1].Where(char.IsDigit)));
+
+            int count = 0;
+            for (long heldTime = 1; heldTime <= time; heldTime++)
+                if (heldTime * (time - heldTime) > distance)
+                    count++;
+                else if (count >= 1)
+                    break; // for performance
+            Console.WriteLine(count);
+        }
+    }
+}
